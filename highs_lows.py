61a04cbb8f557bce6d0b65c6fdc89bf3f9b288d4
Bojan Kalicanin <kalcho@zoho.com>
2016-12-26 22:21:44 +0100

    added solution to the 16.2

--- highs_lows.py
+++ highs_lows.py
@@ -7,29 +7,33 @@ from matplotlib import pyplot as plt
 from datetime import datetime
 
 # Get dates, high, and low temperatures from file.
-filename = 'death_valley_2014.csv'
-with open(filename) as f:
-    reader = csv.reader(f)
-    header_row = next(reader)
-    
-    dates, highs, lows = [], [], []
-    for row in reader:
-        try:
-            current_date = datetime.strptime(row[0], '%Y-%m-%d')
-            high = int(row[1])
-            low = int(row[3])
-        except ValueError:
-            print(current_date, 'missing_data')
-        else:
-            dates.append(current_date)
-            highs.append(high)
-            lows.append(low)
+fig = plt.figure(dpi=100, figsize=(10, 9))
+filenames = ['sitka_weather_2014.csv', 'death_valley_2014.csv']
+for filename in filenames:
+    #filename = 'death_valley_2014.csv'
+    with open(filename) as f:
+        reader = csv.reader(f)
+        header_row = next(reader)
+        
+        dates, highs, lows = [], [], []
+        for row in reader:
+            try:
+                current_date = datetime.strptime(row[0], '%Y-%m-%d')
+                high = int(row[1])
+                low = int(row[3])
+            except ValueError:
+                print(current_date, 'missing_data')
+            else:
+                dates.append(current_date)
+                highs.append(high)
+                lows.append(low)
 
-# Plot data.
-fig = plt.figure(dpi=100, figsize=(10, 6))
-plt.plot(dates, highs, c='red', alpha=0.5)
-plt.plot(dates, lows, c='blue', alpha=0.5)
-plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+    # Plot data.
+    
+    plt.plot(dates, highs, c='red', alpha=0.5)
+    plt.plot(dates, lows, c='blue', alpha=0.5)
+    plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
 
 # Format plot.
 title = "Daily high and low temperatures - 2014\nDeath Valley, CA"
@@ -38,5 +42,6 @@ plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
+plt.axis([None, None, 15, 120])
 
 plt.show()
