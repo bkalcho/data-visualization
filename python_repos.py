6c32696096fbaded913b2d6185b639af24f8059d
Bojan Kalicanin <kalcho@zoho.com>
2016-12-28 12:41:24 +0100

    added python_repos.py

---
+++ python_repos.py
@@ -0,0 +1,31 @@
+# Author: Bojan G. Kalicanin
+# Date: 28-Dec-2016
+# Identify the most starred Python projects on GitHub.
+
+import requests
+
+# Make an API call and store the response.
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print("Status code:", r.status_code)
+
+# Store API response in a variable.
+response_dict = r.json()
+print("Total repositories:", response_dict['total_count'])
+
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
+print("Repositories returned:", len(repo_dicts))
+
+# Examine the first repository.
+repo_dict = repo_dicts[0]
+
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print('Name:', repo_dict['name'])
+    print('Owner:', repo_dict['owner']['login'])
+    print('Stars:', repo_dict['stargazers_count'])
+    print('Repository:', repo_dict['html_url'])
+    print('Created:', repo_dict['created_at'])
+    print('Updated:', repo_dict['updated_at'])
+    print('Description:', repo_dict['description'])
