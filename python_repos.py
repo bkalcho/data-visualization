fcc7a88de5376e34fd53ba92d541db9acaae425a
Bojan Kalicanin <kalcho@zoho.com>
2016-12-28 19:04:15 +0100

    added interactive tooltip

--- python_repos.py
+++ python_repos.py
@@ -19,17 +19,36 @@ print("Total repositories:", response_dict['total_count'])
 repo_dicts = response_dict['items']
 #print("Repositories returned:", len(repo_dicts))
 
+#names, plot_dicts = [], []
 names, stars = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
+    #plot_dict = {
+    #    'value': repo_dict['stargazers_count'],
+    #    'label': repo_dict['description'],
+        #'xlink': repo_dict['html_url'],
+    #    }
+    #plot_dicts.append(plot_dict)
 
 # Make visualization.
 my_style = LS('#333366', base_style=LCS)
-chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
+#chart.add('', plot_dicts)
 chart.add('', stars)
 chart.render_to_file('python_repos.svg')
 # Examine the first repository.
